55b19ccdd9204f6f7b31f770196a9330520fcfc0
Claire Walton <crdewey@hotmail.com>
2020-06-04 17:20:54 +0100

    some problem with push hope this works

--- plot3.R
+++ plot3.R
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 library("readr")
 
 #read in the data
@@ -34,3 +35,41 @@ legend("topright", lty=1, col=c("black","red","blue"), legend = c("Sub_metering_
 
 dev.off()
 
+=======
+library("readr")
+
+#read in the data
+
+power <- read_csv2("household_power_consumption.txt")
+
+# look at Date
+str(power$Date)
+
+# select data from the dates 2007-02-01 and 2007-02-02
+
+febdata <- power[as.Date(power$Date, "%d/%m/%Y")<=as.Date("02/02/2007", "%d/%m/%Y") & as.Date(power$Date, "%d/%m/%Y")>=as.Date("01/02/2007", "%d/%m/%Y") ,]
+
+#create datetime variable
+
+febdata$datetime <- strptime(paste(febdata$Date, febdata$Time), format="%d/%m/%Y %H:%M:%S")
+
+#look at datetime 
+str(febdata$datetime)
+
+
+# create plot3.png 
+
+
+
+
+png("plot3.png", width = 480, height = 480)
+
+plot(febdata$datetime,febdata$Sub_metering_1, type='l',ylab = "Energy sub metering", xlab="")
+lines(febdata$datetime,febdata$Sub_metering_2, col="red")
+lines(febdata$datetime,febdata$Sub_metering_3, col="blue")
+legend("topright", lty=1, col=c("black","red","blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+
+dev.off()
+
+>>>>>>> 5612df370900a71651f31b3151f6b516541da0dd
